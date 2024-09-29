abebc3f8b9c889b919eff84d54ac427859cda52a
Gnanasekar-App <gnanasekar1042@gmail.com>
2024-09-29 23:54:51 +0530

    Add files via upload

---
+++ greenleave/src/main/java/com/green/greenleave/GreenleaveApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenleave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenleaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenleaveApplication.class, args);
+	}
+
+}
---
+++ greenleave/src/test/java/com/green/greenleave/GreenleaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greenleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenleaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
